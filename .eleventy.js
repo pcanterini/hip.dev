c8827eb9fb35fdce6f5ddb6c2fa53acd0f7db7f8
Pedro Canterini <550411+pcanterini@users.noreply.github.com>
2024-07-24 20:18:39 -0700

    add basic theme

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
-
+  eleventyConfig.addWatchTarget("./src/css");
   return {
     dir: {
       input: "src",
